5d6d2e2a9b74a788e981fe1159127e00daf05202
Hassan Aziz <47973@students.riphah.edu.pk>
2024-06-28 19:13:37 +0500

    implemented Posts app models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = ""
+        managed = True
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
