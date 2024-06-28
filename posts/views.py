faac1dc1affd4072641c70a9859c63c566f5fd35
Hassan Aziz <47973@students.riphah.edu.pk>
2024-06-28 19:34:31 +0500

    setup a django-user and loaded data dynamically from post app

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def posts_list(request):
-    return render(request, "posts/posts_list.html")      # first accessing the posts namespace in the templates and then the html
+    # return render(request, "posts/posts_list.html")      # first accessing the posts namespace in the templates and then the html
+    posts = Post.objects.all().order_by("date")
+    return render(request, "posts/posts_list.html", {"posts": posts})
